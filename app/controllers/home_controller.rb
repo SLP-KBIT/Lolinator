437623a9b450b3c360c01566b55f63911aa4069b
shiotomo <shiotomo@boulez2.eng.kagawa-u.ac.jp>
2016-06-12 16:29:20 +0900

    add feature method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,18 @@ class HomeController < ApplicationController
 
   def result
     # binding.pry
-    question = params["question"]
+    # question = params["question"]
+    question = {
+      "innocence" => 5,
+      "high-handed" => 2,
+      "intelligence" => 5,
+      "moment" => 9,
+      "tension" => 8,
+      "hot" => 0,
+      "cold" => 0,
+      "happiness" => 0,
+      "maternal" => 0
+    }
     @features = Feature.calc_scales(question)
   end
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,6 +1,22 @@
 class Feature < ActiveRecord::Base
-
   def self.calc_scales(scales)
-    return scales
+    binding.pry
+     features = {}
+      # features.push("該当するfeature") if ("属性に一致するパラメタが来てるか確認")
+       features.push("edgy") if ( scales["innocence"] >= 5 && scales["high-headed"] >= 2 && scales["intelligence"] >= 5 && scales["tension"] >= 8 )
+       features.push("hot and cold") if ( scales["cold"] == 10 && scales["hot"] == 10 )
+                                                                         
+       features.push("mental health") if ( scales["cold"] >= 3 && scales["hot"] == 3 && scales["moment"] == 10 && scales["happiness"] == 0 )
+       features.push("innocence") if ( scales["innocence"] == 10 && scales["high-headed"] >= 5 && scales["intellingence"] >= 2 )
+       features.push("bitch") if ( scales ["innocence"] == 0 && scales["high-headed"] == 10 && scales["happiness"] == 8 && scales["cold"] >= 6 && scales["hot"] >= 2 && scales["moment"] >=5 && scales["happiness"] == 8 && scales["maternal"] == 0 && scales["tension"] >= 9 )
+       features.push("babumi") if ( scales["maternal"] == 10 && scales["tension"] == 10 )
+       features.push("ajo child") if ( scales ["innocence"] >= 8 && scales["interlligence"] == 0 && scales["happiness"] >= 8 && scales["tension"] == 10 )
+       features.push("cool") if ( scales["innoccence"] == 0 && scales["high-headed"] >= 3 && scales["interlligence"] >= 9 && scales["hot"] >= 6 && scales["moment"] >= 8 && scales["happiness"] >= 1 && scales["tension"] == 0 )
+       features.push("high-headed") if ( scales["high-headed"] == 10 )
+       features.push("flagile") if ( scales["moment"] == 10 && scales["happiness"] <= 3 )
+       features.push("stable") if ( scales ["innocence"] == 0 && scales["interlligence"] >= 3 && scales["tension"] == 0 )
+       features.push("energy") if ( scales ["innocence"] >= 7 && scales[ "happiness" ] >= 5 && scales[ "tension" ] == 10 )
+
+       return features
   end
 end
